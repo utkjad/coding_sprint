d9e957a9472f7ebb850541bd9bd02c760505a1d5
utkjad <jadhav.utkarsh@gmail.com>
2016-03-20 23:31:34 -0400

    Training model is done

--- inventory.py
+++ inventory.py
@@ -13,7 +13,6 @@ class Inventory(object):
 	"""class managing Inventory for Products"""
 	def __init__(self):
 		""" Read path to the csv files and loads data into respective dictionaries"""
-
 		# Read Products
 		self.products = {}
 		with open('products.csv', 'r') as products_fp:
@@ -31,7 +30,7 @@ class Inventory(object):
 				self.ingredients[ingredient_id] = {
 					'price_per_unit':int(ingredient_price_per_unit),
 					'min_order_size': int(ingredient_min_order_size)}
-
+		# print (self.ingredients)
 		# Read Sales
 		with open('sales.csv', 'r') as sales_fp:
 			temp_sales = list(csv.DictReader(sales_fp))
@@ -56,6 +55,8 @@ class Inventory(object):
 			self.inventory_history = map(lambda x: {'ingredient_id': x['ingredient_id'],
 				'day': int(x['day']),
 				'units_in_inventory': int(x['units_in_inventory'])},temp_inventory_history)
+		# print ("HARRY POTTER")
+		# print self.inventory_history
 
 	def get_top_products(self, rank=5, day=9):
 		""" Method which gives list of n top selling products on a particular day.
@@ -75,11 +76,20 @@ class Inventory(object):
 			else:
 				continue
 		result = []
-		for product in sorted(product_revenue.items(), key=operator.itemgetter(1), reverse = True)[:rank]:
+		for product in sorted(product_revenue.items(), key=operator.itemgetter(1), reverse = True):
 			result.append({'product' : {'id' : product[0], 'price' : self.products[product[0]]}, 'revenue': product[1]})
 			# x = {'product' : {'id' : product[0], 'price' : self.products[product[0]]}, 'revenue': product[1]}
 			# print x
-		return result
+		return result[:rank]
+
+	def __get_product_revenues_till_now(self):
+		product_revenue = clt.defaultdict(int)
+		for transaction in self.sales:
+			if transaction['product_id'] in self.products.keys():
+				product_revenue[transaction['product_id']] += (transaction['quantity_sold'] * self.products[transaction['product_id']])
+			else:
+				continue
+		return product_revenue
 
 	def zipcode_revenue_mapping(self, range_of_days):
 		""" Method mapping zipcode with range of days [start, end]
@@ -99,7 +109,7 @@ class Inventory(object):
 				zipcode_revenue[transaction['zipcode']] += (transaction['quantity_sold'] * self.products[transaction['product_id']])
 		return [{'zipcode': x[0], 'revenue':x[1] } for x in zipcode_revenue.iteritems()]
 
-	def depletion_rate(self, ingredient):
+	def get_depletion_rate(self, ingredient):
 		""" Calculates the depletion rate of the ingredient w.r.t. ingredient history only
 
 		Arguments:
@@ -108,21 +118,104 @@ class Inventory(object):
 		Returns:
 			Rate of depletion of given ingredient
 		"""
+		rate_of_depletion = 0.0
 		inventory_record = []
 		# ingredient_id,day,units_in_inventory
-		for example in self.ingredients:
-			if exmaple['ingredient_id'] == ingredient:
-				inventory_record.append({'day': example['day'],
-					'units_in_inventory':exmaple['units_in_inventory']})
+		for example in self.inventory_history:
+			if example['ingredient_id'] == ingredient:
+				inventory_record.append({'day': example['day'], 'units_in_inventory': example['units_in_inventory']})
 
-		inventory_sorted_by_timeline = [sorted(inventory_record, key=itemgetter('day'), reverse = True)]
+		inventory_sorted_by_timeline = [(i['day'], i['units_in_inventory'])
+			for i in sorted(inventory_record, key=itemgetter('day'))]
 
-		# Now, Split history into time intervals and get the average
+		# print inventory_sorted_by_timeline
+		start_day = 0
+		count = 0 
+		min_order_size = self.ingredients[ingredient]['min_order_size']
+		# print ("min_order_size" +  str(min_order_size))
+		for index, tup in enumerate(inventory_sorted_by_timeline):
+			if tup[0] == 0:
+				# print "first step babby"
+				# print "vontinue"
+				continue
+			elif tup[1] == min_order_size:
+				# get previous units in inventory and end day
+				end_day = inventory_sorted_by_timeline[index - 1][0]
+				# print "end day " +  str(end_day)
+				if count > 0:
+					count += 1
+					current_rate = float(min_order_size - inventory_sorted_by_timeline[index - 1][1])/ float(end_day - start_day + 1)
+					rate_of_depletion = ((rate_of_depletion * (count - 1)) + current_rate)/ count
+					start_day = inventory_sorted_by_timeline[index][0]
+					# print rate_of_depletion
+				elif count == 0:
+					# print "count 0 "
+					# print "start day " + str(start_day)
+					# print "last value " + str(inventory_sorted_by_timeline[index - 1][1])
+					# print "min_order_size " + str(min_order_size)
+					count = 1
+					rate_of_depletion = float(min_order_size - inventory_sorted_by_timeline[index - 1][1])/ float(end_day - start_day + 1)
+					# print ("harry")
+					# print rate_of_depletion
+					start_day = inventory_sorted_by_timeline[index][0]
+					# print rate_of_depletion
+			else:
+				# print " continue"
+				continue
+		# print "rate_of_depletion" + str(rate_of_depletion)
+		return rate_of_depletion
 
-		for day_transaction in inventory_sorted_by_timeline:
-			
+	def profile_ingredients(self):
+		""" 
+		"""
+		# Filter all ingredients which are already full. May be not. This is because of min_order_size is minimum, 
+		# not upper bound
+
+		# Now, discard ingredients which are not required in any product
+		ingredients_in_use = set()
+		for record in self.mapping:
+			ingredients_in_use.add(record['ingredient_id'])
+
+		profiler_ingredient = [] # profiler for ingredients
+		product_ingredients = clt.defaultdict(dict) # dict for products and its ingredients
+
+		for ingredient in ingredients_in_use:
+			temp_product = set()
+			for record in self.mapping:
+				product_ingredients[record['product_id']][record['ingredient_id']] = record['quantity']
+				if record['ingredient_id'] == ingredient:
+					temp_product.add(record['product_id'])
+
+			# GET USABILITY FACTOR i.e. in how many products the ingredient was used
+			usability = len(temp_product)
+
+			# GET REVENUE FACTOR
+			# how much revenue the ingredient has contributed so far in all products?
+			product_revenue_list = self.__get_product_revenues_till_now()
+
+			# get total product content (sum of all contents), and contribution of 
+			product_totl_content = {}
+			contribution = 0.0
+			for product in temp_product:
+				product_totl_content[product] = sum(product_ingredients[product].values())
+				contribution += product_revenue_list[product] * float(product_ingredients[product][ingredient])/ float(product_totl_content[product])
+			revenue_factor = contribution
+
+			# GET COST FACTOR
+			cost_factor = self.ingredients[ingredient]['price_per_unit']
+
+			profiler_ingredient.append({'ingredient': ingredient,
+				'rate':self. get_depletion_rate(ingredient),
+				'usability': usability,
+				'revenue_factor': revenue_factor,
+				'cost_factor': cost_factor})
+		return profiler_ingredient
+		
 
 if __name__ == '__main__':
 	i = Inventory()
 	# print i.get_top_products()
-	print i.zipcode_revenue_mapping((0,5))
+	# print i.zipcode_revenue_mapping((0,5))
+	# print i.get_depletion_rate('4e7ddd38')
+	for i in i.profile_ingredients():
+		print i
